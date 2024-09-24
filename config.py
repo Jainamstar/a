2362c2351bccf7e5d9d6df0a8eeffece79330fc8
Jainamstar <mazziniboy41@gmail.com>
2024-09-24 19:34:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7208298886:AAG1zQ2l4KmJ-mcn7DZkKsKbbwqx3ut9u4M")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("jainam2612")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5006333904")
+
+
+
+
+
+
+
+  
